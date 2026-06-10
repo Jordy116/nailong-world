7258b2f28b21786aa8422f4259dcc9da3a3b59c2
Nailong Developer <developer@nailong.world>
2026-06-10 15:22:10 +0800

    🐛 Fix: restore items import for LazyColumn

--- app/src/main/java/com/nailong/world/ui/home/HomeScreen.kt
+++ app/src/main/java/com/nailong/world/ui/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
